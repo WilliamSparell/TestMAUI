63ed3227fa2ae07e67c7f698cba25cd149222a6a
williamsparell <williamatlasnova@gmail.com>
2022-10-21 11:57:50 +0200

    Dial

--- TestMAUI/MainPage.xaml.cs
+++ TestMAUI/MainPage.xaml.cs
@@ -2,23 +2,55 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
+	string translatedNumber;
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count+=5;
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+        if (!string.IsNullOrWhiteSpace(translatedNumber))
+        {
+            callButton.IsEnabled = true;
+            callButton.Text = "Call " + translatedNumber;
+        }
+        else
+        {
+            callButton.IsEnabled = false;
+            callButton.Text = "Call";
+        }
+    }
+    
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        if (await this.DisplayAlert(
+            "Dial a Number",
+            "Would you like to call" + translatedNumber + "?",
+            "Yes",
+            "No"
+            ))
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                    PhoneDialer.Default.Open(translatedNumber);
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await DisplayAlert("Unable to dial", "Phone dialing is not supported on this device.", "OK");
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
-
